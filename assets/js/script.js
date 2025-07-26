4cd2105266efa1206b7308840ff8fea5e0ae018f
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-07-26 18:21:41 +0000

    Add DOMContentLoaded to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-function showSidebar() {
+// Wait for the DOM to be fully loaded before executing the script
+document.addEventListener("DOMContentLoaded", function() {
+
+// Function to toggle the sidebar visibility
+  function showSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = "flex";
 }
 
+// Function to hide the sidebar
 function hideSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = "none";
 }
+
+})
